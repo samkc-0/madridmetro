debaa09967600165ccaf91e8fc95c81a038c4080
samkc-0 <sam.k.c.0@icloud.com>
2025-02-19 13:12:20 +0100

    add metro lines 4 and 5

--- src/Metro/madrid.ts
+++ src/Metro/madrid.ts
@@ -1,7 +1,9 @@
 import { LineNumber, MetroLine, MetroNetwork } from "./classes";
 
-const madridLines: MetroLine[] = ["1", "2", "3", "6", "8"].map((lineNumber) => {
-  return new MetroLine(lineNumber as LineNumber);
-});
+const madridLines: MetroLine[] = ["1", "2", "3", "4", "5", "6", "8"].map(
+  (lineNumber) => {
+    return new MetroLine(lineNumber as LineNumber);
+  }
+);
 
 export const madrid = new MetroNetwork(madridLines);
